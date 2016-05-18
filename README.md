d5412b1b4cc1b4a7dfc07ef51f6a6aba3d56bbbb
Barrett Hafner <barrett@hafnerindustries.com>
2016-05-17 22:49:02 -0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # youtube-browser
-A simple app for searching and browsing youtube videos.
+An app for searching and browsing youtube videos.
+
+## Dependencies
+* [Bootstrap v3.3.6](https://getbootstrap.com/)
+* [Litebox-js](https://github.com/barretthafner/litebox-js)
+* A YouTube API key
+
+## Backstory
+Built for a bootcamp exercise, this app searches the YouTube API based upon a simple search term.
+I set out to do this task with the following requirements:
+1. No jQuery
+2. Display the videos in a lightbox
+3. Keep my YouTube API key out of my repository
+
+To complete requirements 1 and 2 together, I had to build a small lightweight lightbox library ([litebox-js](https://github.com/barretthafner/litebox-js)) that does not require jQuery.
+
+
+## Demo
+
+Try out a demo here: https://youtube-browser.herokuapp.com/
--- litebox.js
+++ litebox.js
@@ -1,8 +1,7 @@
-// litebox
-// next features:
-// use "data-litebox" instead of "data-youtube-id" requires RegEx fun times
-// do other things in litebox: img, html, vimeo
-
+// litebox.js
+// A light-weight, dependency-free light box Javascript library library.
+// Author: Barrett Hafner
+// License: MIT
 
 ;(function() {
 
@@ -110,6 +109,47 @@
     function hideLitebox() {
       liteboxOverlay.style.display = 'none';
       liteboxContent.innerHTML = '';
+    }
+
+    function getYoutubeId(url) {
+      // shamelessly stolen from lity.js
+      // var youtubeRegex = /(youtube(-nocookie)?\.com|youtu\.be)\/(watch\?v=|v\/|u\/|embed\/?)?([\w-]{11})(.*)?/i;
+
+        // For testing.
+      var urls = [
+          '//www.youtube-nocookie.com/embed/up_lNV-yoK4?rel=0',
+          'http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo',
+          'http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel',
+          'http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub',
+          'http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I',
+          'http://www.youtube.com/user/SilkRoadTheatre#p/a/u/2/6dwqZw0j_jY',
+          'http://youtu.be/6dwqZw0j_jY',
+          'http://www.youtube.com/watch?v=6dwqZw0j_jY&feature=youtu.be',
+          'http://youtu.be/afa-5HQHiAs',
+          'http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo?rel=0',
+          'http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel',
+          'http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub',
+          'http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I',
+          'http://www.youtube.com/embed/nas1rJpm7wY?rel=0',
+          'http://www.youtube.com/watch?v=peFZbP64dsU',
+          'http://youtube.com/v/dQw4w9WgXcQ?feature=youtube_gdata_player',
+          'http://youtube.com/vi/dQw4w9WgXcQ?feature=youtube_gdata_player',
+          'http://youtube.com/?v=dQw4w9WgXcQ&feature=youtube_gdata_player',
+          'http://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtube_gdata_player',
+          'http://youtube.com/?vi=dQw4w9WgXcQ&feature=youtube_gdata_player',
+          'http://youtube.com/watch?v=dQw4w9WgXcQ&feature=youtube_gdata_player',
+          'http://youtube.com/watch?vi=dQw4w9WgXcQ&feature=youtube_gdata_player',
+          'http://youtu.be/dQw4w9WgXcQ?feature=youtube_gdata_player'
+      ];
+
+      var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+
+      for (i = 0; i < urls.length; ++i) {
+          var r = urls[i].match(rx);
+          console.log(r[1]);
+      }
+
+
     }
   }, false); // DOMContentLoaded event listener
 }()); // end of IIFE
